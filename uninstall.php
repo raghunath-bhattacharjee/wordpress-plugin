78176f31830cc78f160900e243c2ead579ade86c
Raghunath Bhattacharjee <41074356+CodifyCode2018@users.noreply.github.com>
2019-01-19 21:07:21 +0530

    delete the post from wp database type selection

--- uninstall.php
+++ uninstall.php
@@ -9,16 +9,16 @@ if( ! defined('WP_UNINSTALL_PLUGIN') ){
 	die;
 }
 
-//clear databse data
+//clear databse data type:1
 $books = get_posts( array('post_type' => 'book','numberposts' => -1) );
 
 foreach ($books as $book) {
 	wp_delete_post( $book->ID, true);
 }
 
-//Access the databse via sql and delete
+//Access the databse via sql and delete type:2
 
-global $wpdb;
-$wpdb->query( "DELETE FROM wp_posts WHERE posts_type = 'book'" );
+//global $wpdb;
+//$wpdb->query( "DELETE FROM wp_posts WHERE posts_type = 'book'" );
 
 ?>
